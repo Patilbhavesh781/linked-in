f91781b9df1ccb97274f7afcb5e55198aa0bc5ef
Bhavesh Patil <patilbhavesh781@gmail.com>
2026-01-19 16:53:26 +0530

    backend connected to MongoDB

--- linkedin-backend/connection.js
+++ linkedin-backend/connection.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI).then(res=>{
     console.log("MongoDB Succesfully Connected")
 }).catch(err=>{
-    console.log(`Error is here ${err}`)
+    console.log(err)
 })
