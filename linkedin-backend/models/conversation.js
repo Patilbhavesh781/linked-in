c81329c7495b2520261f656361b2350d0a7cb314
Bhavesh Patil <patilbhavesh781@gmail.com>
2026-01-28 15:38:03 +0530

    MongoDB Models Deployed

---
+++ linkedin-backend/models/conversation.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const ConversationSchema = new mongoose.Schema({
+    members: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user"
+        }
+    ]
+},{
+    timestamps: true
+});
+
+const ConversationModel = mongoose.model('conversation', ConversationSchema);
+module.exports = ConversationModel;
