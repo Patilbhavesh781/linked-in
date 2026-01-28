3945e435502a3ea150244518e88e9c7dc03a037d
Bhavesh Patil <patilbhavesh781@gmail.com>
2026-01-28 14:59:05 +0530

    Models Added

---
+++ linkedin-backend/models/user.js
@@ -0,0 +1,95 @@
+const mongoose = require('mongoose');
+const { type } = require('node:os');
+
+const UserSchema = new mongoose.Schema({
+    googleId: {
+        type: String,
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+    },
+    f_name: {
+        type: String,
+        default: ""
+    },
+    headline: {
+        type: String,
+        default: ""
+    },
+    curr_company: {
+        type: String,
+        default: ""
+    },
+    curr_location: {
+        type: String,
+        default: ""
+    },
+    profilePic: {
+        type: String,
+        default: "https://res.cloudinary.com/mashhuudanny/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1747042975/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9_c6eolb.jpg"
+    },
+    cover_pic: {
+        type: String,
+        default: "https://wallpaperaccess.com/full/6060285.png"
+    },
+    about: {
+        type: String,
+        default: ""
+    },
+    skills: {
+        type: [String],
+        default: []
+    },
+    experience: [
+        {
+            designation: {
+                type: String,
+                default: ""
+            },
+            company_name: {
+                type: String,
+                default: ""
+            },
+            duration: {
+                type: String,
+                default: ""
+            },
+            location: {
+                type: String,
+                default: ""
+            }
+        }
+    ],
+    
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user',
+        }
+    ],
+
+    pending_friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user',
+        }
+    ],
+
+    resume: {
+        type: String,
+    },
+
+},
+{
+    timestamps: true
+});
+
+const userModel = mongoose.model('user', UserSchema);
+
+module.exports = userModel;
+
